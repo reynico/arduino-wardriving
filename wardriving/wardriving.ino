89efdcc6e8e4150e2be4b3adea288ff37a8e06c0
Nicolas Rey <rey.nico@gmail.com>
2018-04-04 00:33:14 -0300

    Major refactor/rework on code

--- wardriving/wardriving.ino
+++ wardriving/wardriving.ino
@@ -1,9 +1,9 @@
-
-#include <SPI.h>
 #include <SD.h>
+#include <SPI.h>
+#include <Wire.h>
 #include <TinyGPS++.h>
 #include "ESP8266WiFi.h"
-#include <Wire.h>
+#include <SoftwareSerial.h>
 #include <LiquidCrystal_I2C.h>
  
 LiquidCrystal_I2C lcd(0x27, 16, 2);
@@ -12,29 +12,26 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 #define MAX_LOG_FILES 100
 #define LOG_FILE_SUFFIX "csv" 
 char logFileName[13];
-#define LOG_COLUMN_COUNT 13
+#define LOG_COLUMN_COUNT 11
+const String wigleHeaderFileFormat = "WigleWifi-1.4,appRelease=2.26,model=Feather,release=0.0.0,device=arduinoWardriving,display=3fea5e7,board=esp8266,brand=Adafruit";
 char * log_col_names[LOG_COLUMN_COUNT] = {
-  "Latitude", "Longitude", "Altitude", "Speed", "Course", "Date", "Time (UTC)", "Satellites", "SSID", "Power", "Channel", "Encryption", "BSSID"
+  "MAC", "SSID", "AuthMode", "FirstSeen", "Channel", "RSSI", "CurrentLatitude", "CurrentLongitude", "AltitudeMeters", "AccuracyMeters", "Type"
 };
 
 #define LOG_RATE 2000
 unsigned long lastLog = 0;
 
+static const int RXPin = 0, TXPin = 3;
+static const uint32_t GPSBaud = 9600;
+SoftwareSerial ss(RXPin, TXPin);
 TinyGPSPlus tinyGPS;
-#define GPS_BAUD 9600 // GPS module's default baud rate
-
-#include <SoftwareSerial.h>
-#define ARDUINO_GPS_RX 0 // Pin D4
-#define ARDUINO_GPS_TX 2 // Pin D3
-SoftwareSerial ss(ARDUINO_GPS_RX, ARDUINO_GPS_TX);
-//#define ss ssGPS 
 int display = 1;
 
 #define SerialMonitor Serial
 
 void setup() {
   SerialMonitor.begin(115200);
-  ss.begin(GPS_BAUD);
+  ss.begin(GPSBaud);
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
   lcd.init();
@@ -55,55 +52,47 @@ void setup() {
 }
 
 void loop() {
-//  while (ss.available() > 0)
-//    tinyGPS.encode(ss.read());
-    
-  if ((lastLog + LOG_RATE) <= millis()) {
-    if (tinyGPS.location.isUpdated()) {
-      if (logGPSData()) {
-        SerialMonitor.print("GPS logged ");
-        SerialMonitor.print(tinyGPS.location.lat(), 6);
-        SerialMonitor.print(", ");
-        SerialMonitor.println(tinyGPS.location.lng(), 6);
-        SerialMonitor.print("Seen networks: ");
-        SerialMonitor.println(countNetworks());
-        if (display == 1) {
-          lcd.clear();
-          lcd.setCursor(0, 0);
-          lcd.print("Lat: ");
-          lcd.print(tinyGPS.location.lat(), 6);
-          lcd.setCursor(0, 1);
-          lcd.print("Lon: ");
-          lcd.print(tinyGPS.location.lng(), 6);
-          display = 0;
-        } else {
-          lcd.clear();
-          lcd.setCursor(0, 0);
-          lcd.print("Seen: ");
-          lcd.print(countNetworks());
-          lcd.setCursor(0, 1);
-          lcd.print("networks");
-          display = 1;
-        }
-        lastLog = millis();
-      } else {
-        lcd.setCursor(0, 1);
-        SerialMonitor.println("Failed to log new GPS data.");
-      }
-    } else {
-      lcd.setCursor(0, 0);
-      lcd.print("No GPS data");
-      lcd.setCursor(0, 1);
-      lcd.print("Sats: ");
-      lcd.print(tinyGPS.satellites.value());
-      SerialMonitor.print("No GPS data. Sats: ");
-      SerialMonitor.println(tinyGPS.satellites.value());
-      delay(100);
-    }
+  SerialMonitor.print("GPS logged ");
+  SerialMonitor.print(tinyGPS.location.lat(), 6);
+  SerialMonitor.print(", ");
+  SerialMonitor.println(tinyGPS.location.lng(), 6);
+  SerialMonitor.print("Seen networks: ");
+  SerialMonitor.println(countNetworks());
+  if (tinyGPS.location.isValid()) {
+    logGPSData();
   }
-  while (ss.available())
-    tinyGPS.encode(ss.read());
+  if (display == 1) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Lat: ");
+    lcd.print(tinyGPS.location.lat(), 6);
+    lcd.setCursor(0, 1);
+    lcd.print("Lon: ");
+    lcd.print(tinyGPS.location.lng(), 6);
+    display = 0;
+  } else {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Seen: ");
+    lcd.print(countNetworks());
+    lcd.setCursor(0, 1);
+    lcd.print("networks");
+    display = 1;
+  }
+  lastLog = millis();
+  smartDelay(1000);
+  if (millis() > 5000 && tinyGPS.charsProcessed() < 10)
+    SerialMonitor.println("No GPS data received: check wiring");
 }
+
+static void smartDelay(unsigned long ms) {
+  unsigned long start = millis();
+  do {
+    while (ss.available())
+      tinyGPS.encode(ss.read());
+  } while (millis() - start < ms);
+}
+
 int countNetworks() {
   File netFile = SD.open(logFileName);
   int networks = 0;
@@ -120,6 +109,7 @@ int countNetworks() {
     }
   }
 }
+
 int isOnFile(String mac) {
   File netFile = SD.open(logFileName);
   String currentNetwork;
@@ -127,9 +117,9 @@ int isOnFile(String mac) {
     while(netFile.available()) {
       currentNetwork = netFile.readStringUntil('\n');
       if (currentNetwork.indexOf(mac) != -1) {
-        SerialMonitor.println("The network was already found");
-        netFile.close();
-        return currentNetwork.indexOf(mac);
+  SerialMonitor.println("The network was already found");
+  netFile.close();
+  return currentNetwork.indexOf(mac);
       }
     }
     netFile.close();
@@ -144,44 +134,41 @@ byte logGPSData() {
   } else {
     for (uint8_t i = 1; i <= n; ++i) {
       if ((isOnFile(WiFi.BSSIDstr(i)) == -1) && (WiFi.channel(i) > 0) && (WiFi.channel(i) < 15)) { //Avoid erroneous channels
-        File logFile = SD.open(logFileName, FILE_WRITE);
-        SerialMonitor.println("New network found");
-        logFile.print(tinyGPS.location.lat(), 6);
-        logFile.print(',');
-        logFile.print(tinyGPS.location.lng(), 6);
-        logFile.print(',');
-        logFile.print(tinyGPS.altitude.meters(), 1);
-        logFile.print(',');
-        logFile.print(tinyGPS.speed.kmph(), 1);
-        logFile.print(',');
-        logFile.print(tinyGPS.course.deg(), 1);
-        logFile.print(',');
-        logFile.print(tinyGPS.date.month());
-        logFile.print('/');
-        logFile.print(tinyGPS.date.day());
-        logFile.print('/');
-        logFile.print(tinyGPS.date.year());
-        logFile.print(',');
-        logFile.print(tinyGPS.time.hour());
-        logFile.print(':');
-        logFile.print(tinyGPS.time.minute());
-        logFile.print(':');
-        logFile.print(tinyGPS.time.second());
-        logFile.print(',');
-        int sat = tinyGPS.satellites.value();
-        logFile.print(sat);
-        logFile.print(',');
-        logFile.print(WiFi.SSID(i));
-        logFile.print(',');
-        logFile.print(WiFi.RSSI(i));
-        logFile.print(',');
-        logFile.print(WiFi.channel(i));
-        logFile.print(',');
-        logFile.print(getEncryption(i));
-        logFile.print(',');
-        logFile.print(WiFi.BSSIDstr(i));
-        logFile.println();
-        logFile.close();
+  File logFile = SD.open(logFileName, FILE_WRITE);
+  SerialMonitor.println("New network found");
+  logFile.print(WiFi.BSSIDstr(i));
+  logFile.print(',');
+  logFile.print(WiFi.SSID(i));
+  logFile.print(',');
+  logFile.print(getEncryption(i));
+  logFile.print(',');
+  logFile.print(tinyGPS.date.year());
+  logFile.print('-');
+  logFile.print(tinyGPS.date.month());
+  logFile.print('-');
+  logFile.print(tinyGPS.date.day());
+  logFile.print(' ');
+  logFile.print(tinyGPS.time.hour());
+  logFile.print(':');
+  logFile.print(tinyGPS.time.minute());
+  logFile.print(':');
+  logFile.print(tinyGPS.time.second());
+  logFile.print(',');
+  logFile.print(WiFi.channel(i));
+  logFile.print(',');
+  logFile.print(WiFi.RSSI(i));
+  logFile.print(',');
+  logFile.print(tinyGPS.location.lat(), 6);
+  logFile.print(',');
+  logFile.print(tinyGPS.location.lng(), 6);
+  logFile.print(',');
+  logFile.print(tinyGPS.altitude.meters(), 1);
+  logFile.print(',');
+  logFile.print((tinyGPS.hdop.value(), 1));
+  logFile.print(',');
+  logFile.print("WIFI");
+  logFile.println();
+  logFile.close();
       }
     }
   }
@@ -191,12 +178,13 @@ void printHeader() {
   File logFile = SD.open(logFileName, FILE_WRITE);
   if (logFile) {
     int i = 0;
+    logFile.println(wigleHeaderFileFormat);
     for (; i < LOG_COLUMN_COUNT; i++) {
       logFile.print(log_col_names[i]);
       if (i < LOG_COLUMN_COUNT - 1)
-        logFile.print(',');
+  logFile.print(',');
       else
-        logFile.println();
+  logFile.println();
     }
     logFile.close();
   }
@@ -222,14 +210,14 @@ String getEncryption(uint8_t network) {
   byte encryption = WiFi.encryptionType(network);
   switch (encryption) {
     case 2:
-      return "WPA (TKIP)";
+      return "[WPA-PSK-CCMP+TKIP][ESS]";
     case 5:
-      return "WEP";
+      return "[WEP][ESS]";
     case 4:
-      return "WPA (CCMP)";
+      return "[WPA2-PSK-CCMP+TKIP][ESS]";
     case 7:
-      return "NONE";
+      return "[ESS]";
     case 8:
-      return "AUTO";
+      return "[WPA-PSK-CCMP+TKIP][WPA2-PSK-CCMP+TKIP][ESS]";
   }
 }
